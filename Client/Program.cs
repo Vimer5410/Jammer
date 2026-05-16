5c68e5b02060b03d2d51d6bb877504189d7b2563
Vimer5410 <gleb.dubinin@rambler.ru>
2026-05-16 18:56:36 +0300

    first commit :D

---
+++ Client/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        byte[] buffer = new byte[120];
+        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        var ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1666);
+        await socket.ConnectAsync(ip);
+
+
+        var sendTask = Task.Run(async () =>
+        {
+            try
+            {
+                while (true)
+                {
+                    var data = Console.ReadLine();
+                    await socket.SendAsync(Encoding.UTF8.GetBytes(data));
+                }
+            
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        });
+
+        var receiveTask = Task.Run(async () =>
+        {
+            while (true)
+            {
+                int buffersize = await socket.ReceiveAsync(buffer);
+                var message = Encoding.UTF8.GetString(buffer, 0, buffersize);
+                Console.WriteLine($"New message:{message}");
+            }
+        });
+
+
+        await Task.WhenAll(sendTask, receiveTask);
+
+    }
+}
---
+++ Jammer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        byte[] buffer = new byte[120];
+        var serverip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1666);
+        socket.Bind(serverip);
+        socket.Listen();
+        Socket receive = await socket.AcceptAsync();
+
+
+        
+
+        var receiveTask = Task.Run(async () =>
+        {
+            while (true)
+            {
+                int buffersize= await receive.ReceiveAsync(buffer);
+                var message = Encoding.UTF8.GetString(buffer, 0, buffersize);
+                Console.WriteLine($"New message:{message}");
+            }
+        }); 
+
+        var sendTask = Task.Run(async () =>
+        {
+            var data = Console.ReadLine();
+            await receive.SendAsync(Encoding.UTF8.GetBytes(data));
+        });
+
+
+        await Task.WhenAll(receiveTask, sendTask);
+        
+    }
+}
