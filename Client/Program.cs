a0f205af7c4a7d03ccb4381498bd96da71c99713
Vimer5410 <gleb.dubinin@rambler.ru>
2026-06-09 22:12:34 +0300

    add UDP traffic exchange

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,27 +3,52 @@ using System.ComponentModel;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Client;
 
 class Program
 {
+    private static string serverIp { get; set; }
+    private static int serverPort { get; set; }
+    private static int clientPort { get; set; }
+    private static string? userName { get; set; }
     static async Task Main(string[] args)
     {
-        byte[] buffer = new byte[120];
-        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        var ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1666);
-        await socket.ConnectAsync(ip);
-
-
+        Console.WriteLine("Введите ip сервера:");
+        serverIp = Console.ReadLine() switch { "" or null => "127.0.0.1", string s => s };
+        Console.WriteLine("Введите порт сервера:");
+        serverPort = Convert.ToInt32(Console.ReadLine() switch { "" or null => "7777", string s => s });
+        Console.WriteLine("Введите ваш порт:");
+        clientPort = Convert.ToInt32(Console.ReadLine() switch{"" or null => "8888", string s=>s});
+        Console.WriteLine("Введите ваше имя:");
+        
+        string hostName = Dns.GetHostName();
+        IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+        
+        AddressFamily myIp = AddressFamily.Unspecified;
+        
+        foreach (var ip in addresses)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                Console.WriteLine(ip);
+            }   
+        }
+        
+        userName = Console.ReadLine();
+        
         var sendTask = Task.Run(async () =>
         {
             try
             {
                 while (true)
                 {
-                    var data = Console.ReadLine();
-                    await socket.SendAsync(Encoding.UTF8.GetBytes(data));
+                    string input = await Task.Run(() => Console.ReadLine());
+                    if (string.IsNullOrEmpty(input)) continue;
+                    var data = Encoding.UTF8.GetBytes($"[{userName}] "+input);
+                    var sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    await sendSocket.SendToAsync(data, new IPEndPoint(IPAddress.Parse(serverIp), serverPort));
                 }
             
             }
@@ -35,10 +60,13 @@ class Program
 
         var receiveTask = Task.Run(async () =>
         {
+            var receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            receiveSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), clientPort));
+            byte[] buffer = new byte[256];
             while (true)
             {
-                int buffersize = await socket.ReceiveAsync(buffer);
-                var message = Encoding.UTF8.GetString(buffer, 0, buffersize);
+                var data = await receiveSocket.ReceiveFromAsync(buffer, new IPEndPoint(IPAddress.Any, clientPort));
+                var message = Encoding.UTF8.GetString(buffer, 0, data.ReceivedBytes);
                 Console.WriteLine($"New message:{message}");
             }
         });
--- Jammer/Program.cs
+++ Jammer/Program.cs
@@ -2,42 +2,53 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Server;
 
 class Program
 {
-    static async Task Main(string[] args)
+    public static readonly string localIpAddress = "127.0.0.1";
+    public static int localPort { get; set; }
+    
+    public static int port { get; set; }
+    
+    async static Task Main(string[] args)
     {
-        
-        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        byte[] buffer = new byte[120];
-        var serverip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1666);
-        socket.Bind(serverip);
-        socket.Listen();
-        Socket receive = await socket.AcceptAsync();
+        Console.WriteLine("Введите порт для приема сообщений:");
+        localPort = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите порт клиента(для отправки сообщений):");
+        port = Convert.ToInt32(Console.ReadLine());
+        await Task.WhenAll(SendMessageAsync(), ReceiveMessageAsync());
+    }
 
+    async static Task SendMessageAsync()
+    {
+        var sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-        
-
-        var receiveTask = Task.Run(async () =>
+        while (true)
         {
-            while (true)
-            {
-                int buffersize= await receive.ReceiveAsync(buffer);
-                var message = Encoding.UTF8.GetString(buffer, 0, buffersize);
-                Console.WriteLine($"New message:{message}");
-            }
-        }); 
+            
+            string input= await Task.Run(() => Console.ReadLine());
+            if (string.IsNullOrEmpty(input)) continue;
+            
+            var data = Encoding.UTF8.GetBytes(input);
+            
+            await sendSocket.SendToAsync(data, new IPEndPoint(IPAddress.Parse(localIpAddress), port));
+        }
+    }
 
-        var sendTask = Task.Run(async () =>
-        {
-            var data = Console.ReadLine();
-            await receive.SendAsync(Encoding.UTF8.GetBytes(data));
-        });
-
-
-        await Task.WhenAll(receiveTask, sendTask);
+    async static Task ReceiveMessageAsync()
+    {
+        var receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        receiveSocket.Bind(new IPEndPoint(IPAddress.Parse(localIpAddress), localPort));
+        byte[] buffer = new byte[256];
         
+        while (true)
+        {
+            var data = await receiveSocket.ReceiveFromAsync(buffer, new IPEndPoint(IPAddress.Any, 0));
+            var message = Encoding.UTF8.GetString(buffer, 0, data.ReceivedBytes);
+            Console.WriteLine($"New message:{message}");
+        }
     }
 }
