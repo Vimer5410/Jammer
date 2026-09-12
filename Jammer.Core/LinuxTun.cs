ebc7071237fc450dcbd274299985b380ff81f8d6
Vimer5410 <gleb.dubinin@rambler.ru>
2026-09-12 15:01:09 +0300

    write a basic wrapper for linuxtun class

---
+++ Jammer.Core/LinuxTun.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Jammer.Core;
+
+public class LinuxTun
+{
+    //fix: посчитать значения для каждой константы 
+    
+    private const int O_RDWR;
+    
+    //fix: написать готовую формулу для вычисления _IOW без хардкода 0x400454ca
+    private const ulong TUNSETIFF;
+
+    private const short IFF_TUN;
+
+    private const short IFF_NO_PI;
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Ifreq
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] ifrn_name;  //= ifr_ifrn union, 16 байт
+
+        public short ifr_flags; //= первые 2 байта ifr_ifru union
+        
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+        public byte[] padding;  //= оставшиеся 22 байта union
+    }
+    
+    [DllImport("libc", CharSet = CharSet.Ansi, SetLastError = true)]
+    private static extern int ioctl
+    (
+        int fd,
+        ulong op,
+        ref Ifreq ifreq
+    );
+    
+    [DllImport("libc", CharSet = CharSet.Ansi, SetLastError = true)]
+    private static extern int close
+    (
+        int fd
+    );
+    
+    [DllImport("libc", CharSet = CharSet.Ansi, SetLastError = true)]
+    private static extern int open
+    (
+        string pathname,
+        int flags
+    );
+    
+    [DllImport("libc", CharSet = CharSet.Ansi, SetLastError = true)]
+    private static extern int write
+    (
+        int fd,
+        byte[] buffer,
+        ulong count
+    );
+    
+    [DllImport("libc", CharSet = CharSet.Ansi, SetLastError = true)]
+    private static extern int read
+    (
+        int fd,
+        byte[] buffer,
+        ulong count
+    );
+    
+}
